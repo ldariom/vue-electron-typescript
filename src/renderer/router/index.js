a16ba562d8e16ef6617e80315653ace147e479e7
Dario Menéndez <tyris.dario.menendez@gmail.com>
2019-06-06 14:07:23 +0200

    initial commit

---
+++ src/renderer/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'landing-page',
+      component: require('@/components/LandingPage').default
+    },
+    {
+      path: '*',
+      redirect: '/'
+    }
+  ]
+})
